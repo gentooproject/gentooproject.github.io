fbe573c229c9d7373986f3849557325e3257e721
Amurita <switch-on@auroralilium.com>
2017-10-09 06:52:51 +0000

    Added javascripts of the index

---
+++ assets/js/misc.js
@@ -0,0 +1,14 @@
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(alertFunc, 3000);
+}
+
+function alertFunc() {
+if( navigator.userAgent.match(/X11/i)
+  ){
+    if(window.prompt('Veo que usas Linux ❤, ¡muy bien! , te invito especialmente a mi sitio sobre Linux. ¿Deseas visitarlo en este momento?', 'https://daya.cf/go'))
+  location.href='https://daya.cf/go';
+  }
+}
+ myFunction()
