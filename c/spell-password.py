d671691fddb8672d14896ad92d43b1de5837829f
Amurita <switch-on@auroralilium.com>
2017-10-23 02:27:26 +0000

    Add files via upload

---
+++ c/spell-password.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+ # -*- coding: utf-8 -*-
+print "Paste your password"
+text = raw_input()
+print "-----------"
+for idx, c_char in enumerate(text):
+        if c_char == 'a':
+            print  str(idx + 1) + ' '  + c_char + ' lowercase a'
+        if c_char == 'b':
+            print  str(idx + 1) + ' ' + c_char + ' lowercase bee'
+        if c_char == 'c': 
+            print str(idx + 1) + ' ' + c_char +  ' lowercase cee'
+        if c_char == 'd':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase dee'
+        if c_char == 'e':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase e'
+        if c_char == 'f':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase ef'
+        if c_char == 'g':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase gee'
+        if c_char == 'h':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase aitch'
+        if c_char == 'i':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase i'
+        if c_char == 'j':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase jay'
+        if c_char == 'k':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase key'
+        if c_char == 'l':
+            print str(idx + 1) + ' ' +  c_char +  ' lowercase el'
+        if c_char == 'm':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase em'
+        if c_char == 'n':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase en'
+        if c_char == 'o':
+            print str(idx + 1) + ' ' + c_char +  ' lowercase o'
+        if c_char == 'p':
+            print str(idx + 1) + ' ' + c_char + ' lowercase pee'
+        if c_char == 'q':
+            print str(idx + 1) + ' ' + c_char + ' lowercase cue'
+        if c_char == 'r':
+            print str(idx + 1) + ' ' + c_char + ' lowercase ar'
+        if c_char == 's':
+            print str(idx + 1) + ' ' + c_char + ' lowercase es'
+        if c_char == 't':
+            print str(idx + 1) + ' ' + c_char + ' lowercase tee'
+        if c_char == 'u':
+            print str(idx + 1) + ' ' + c_char + ' lowercase u'
+        if c_char == 'v':
+            print str(idx + 1) + ' ' + c_char + ' lowercase vee'
+        if c_char == 'w':
+            print str(idx + 1) + ' ' + c_char + ' lowercase double-u'
+        if c_char == 'x':
+            print str(idx + 1) + ' ' + c_char + ' lowercase ex'
+        if c_char == 'y':
+            print str(idx + 1) + ' ' + c_char + ' lowercase wye'
+        if c_char == 'z':
+            print str(idx + 1) + ' ' + c_char + ' lowercase zed'
+        if c_char == 'A':
+            print str(idx + 1) + ' ' + c_char + ' uppercase a'
+        if c_char == 'B':
+            print str(idx + 1) + ' ' + c_char + ' uppercase bee'
+        if c_char == 'C': 
+            print str(idx + 1) + ' ' + c_char + ' uppercase cee'
+        if c_char == 'D':
+            print str(idx + 1) + ' ' + c_char + ' uppercase dee'
+        if c_char == 'E':
+            print str(idx + 1) + ' ' + c_char + ' uppercase e'
+        if c_char == 'F':
+            print str(idx + 1) + ' ' + c_char + ' uppercase ef'
+        if c_char == 'G':
+            print str(idx + 1) + ' ' + c_char + ' uppercase gee'
+        if c_char == 'H':
+            print str(idx + 1) + ' ' + c_char + ' uppercase aitch'
+        if c_char == 'I':
+            print str(idx + 1) + ' ' + '\033[95m' + c_char + '\033[0m' + ' uppercase i'
+        if c_char == 'J':
+            print str(idx + 1) + ' ' + c_char + ' uppercase jay'
+        if c_char == 'K':
+            print str(idx + 1) + ' ' + c_char + ' uppercase key'
+        if c_char == 'L':
+            print str(idx + 1) + ' ' + c_char + ' uppercase el'
+        if c_char == 'M':
+            print str(idx + 1) + ' ' + c_char + ' uppercase em'
+        if c_char == 'N':
+            print str(idx + 1) + ' ' + c_char + ' uppercase en'
+        if c_char == 'O':
+            print str(idx + 1) + ' ' + '\033[95m' + c_char + '\033[0m' + ' uppercase o'
+        if c_char == 'P':
+            print str(idx + 1) + ' ' + c_char + ' uppercase pee'
+        if c_char == 'Q':
+            print str(idx + 1) + ' ' + c_char + ' uppercase cue'
+        if c_char == 'R':
+            print str(idx + 1) + ' ' + c_char + ' uppercase ar'
+        if c_char == 'S':
+            print str(idx + 1) + ' ' + c_char + ' uppercase es'
+        if c_char == 'T':
+            print str(idx + 1) + ' ' + c_char + ' uppercase tee'
+        if c_char == 'U':
+            print str(idx + 1) + ' ' + c_char + ' uppercase u'
+        if c_char == 'V':
+            print str(idx + 1) + ' ' + c_char + ' uppercase vee'
+        if c_char == 'W':
+            print str(idx + 1) + ' ' + c_char + ' uppercase double-u'
+        if c_char == 'X':
+            print str(idx + 1) + ' ' + c_char + ' uppercase ex'
+        if c_char == 'Y':
+            print str(idx + 1) + ' ' + c_char + ' uppercase wye'
+        if c_char == 'Z':
+            print str(idx + 1) + ' ' + c_char + ' uppercase zed'
+        if c_char == '=':
+            print str(idx + 1) + ' ' + c_char + ' equal to symbol'
+        if c_char == '/':
+            print str(idx + 1) + ' ' + c_char + ' slash'
+        if c_char == '$':
+            print str(idx + 1) + ' ' + c_char + ' money symbol'
+        if c_char == '+':
+            print str(idx + 1) + ' ' + c_char + ' plus symbol'
+        if c_char == '-':
+            print str(idx + 1) + ' ' + c_char + ' hyphen'
+        if c_char == '_':
+            print str(idx + 1) + ' ' + c_char + ' underscore'
+        if c_char == '%':
+            print str(idx + 1) + ' ' + c_char + ' percent sign'
+        if c_char == ':':
+            print str(idx + 1) + ' ' + c_char + ' semicolon'
+        if c_char == ';':
+            print str(idx + 1) + ' ' + c_char + ' colon'
+        if c_char == '\'':
+            print str(idx + 1) + ' ' + c_char + ' apostrophe'
+        if c_char == '1':
+            print str(idx + 1) + ' ' + c_char + ' number one'
+        if c_char == '2':
+            print str(idx + 1) + ' ' + c_char + ' number two'
+        if c_char == '3':
+            print str(idx + 1) + ' ' + c_char + ' number three'
+        if c_char == '4':
+            print str(idx + 1) + ' ' + c_char + ' number four'
+        if c_char == '5':
+            print str(idx + 1) + ' ' + c_char + ' number five'
+        if c_char == '6':
+            print str(idx + 1) + ' ' + c_char + ' number six'
+        if c_char == '7':
+            print str(idx + 1) + ' ' + c_char + ' number seven'
+        if c_char == '8':
+            print str(idx + 1) + ' ' + c_char + ' number eight'
+        if c_char == '9':
+            print str(idx + 1) + ' ' + c_char + ' number nine'
+        if c_char == '0':
+            print str(idx + 1) + ' ' + '\033[36m' + c_char + '\033[0m'  + ' number zero'
+        if c_char == ' ':
+            print str(idx + 1) + ' ' + c_char + ' space'
+
+print
+if text.count('O') >= 1 and text.count('I') >= 1:
+    if text.count('0') >= 1:
+        print "The uppercase letters O and I are purple colored, the zero is cyan colored"
+    else:
+        print "The uppercase letters O and I are purple colored"
+if text.count('O') >= 1 and text.count('I') == 0:
+    if text.count('0') >= 1:
+        print "The uppercase letter O is purple colored, the zero is cyan colored"
+    else:
+        print "The uppercase letter O is purple colored"
+if text.count('I') >= 1 and text.count('O') == 0:
+    if text.count('0') >= 1:
+        print "The uppercase letter I is purple colored, the zero is cyan colored"
+    else:
+        print "The uppercase letter I is purple colored"
+
+
+
