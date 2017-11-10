760359d35cd689d50751a8bdd855be60daaa2ffc
Amurita <switch-on@auroralilium.com>
2017-11-10 00:58:01 +0000

    Update misc.js

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -7,8 +7,18 @@ function myFunction() {
 function alertFunc() {
 if( navigator.userAgent.match(/X11/i)
   ){
-    if(window.prompt('Veo que usas Linux ❤, ¡muy bien! , te invito especialmente a mi sitio ¿Deseas visitarlo?', 'https://pinkhat.rpm.li'))
-  location.href='https://pinkhat.rpm.li';
+    var langbis = navigator.language
+              var lang = langbis.split('-')[0] || [] ;
+              var re = new RegExp("[en,es]");
+              if (re.test(lang)) {
+              } else {
+              var lang = "en" ;
+              }
+
+      $('#linux').load('https://rpm.li/assets/linux-blog.html #' + lang, function() {
+      fnShowLoading(false);
+   // if(window.prompt('Veo que usas Linux ❤, ¡muy bien! , te invito especialmente a mi sitio ¿Deseas visitarlo?', 'https://pinkhat.rpm.li'))
+  //location.href='https://pinkhat.rpm.li';
   }
 }
  myFunction()
